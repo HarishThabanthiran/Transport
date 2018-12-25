308caef6f4231fe08fb8cef498be054c1ad7fbb2
HarishThabanthiran <harishthabanthiran@gmail.com>
2018-12-25 23:17:41 +0530

    removed two methods

--- src/org/test/exp.java
+++ src/org/test/exp.java
@@ -8,18 +8,9 @@ public class exp {
 	public static void flower() {
 		System.out.println("Rose flower");
 	}
-	public static void car() {
-		System.out.println("Scorpio");
-	}
-	
-	public static void bus() {
-		System.out.println("905 BUS");
-	}
 	public static void main(String[] args) {
 		tree();
 		flower();
-		car();
-		bus();
 		
 	}
 	
