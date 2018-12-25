68bd37828a84519980dafc37d613370e013df805
HarishThabanthiran <harishthabanthiran@gmail.com>
2018-12-25 23:10:35 +0530

    added two new methods

--- src/org/test/exp.java
+++ src/org/test/exp.java
@@ -2,6 +2,12 @@ package org.test;
 
 public class exp {
 	
+	public static void tree() {
+		System.out.println("Banyan tree");
+	}
+	public static void flower() {
+		System.out.println("Rose flower");
+	}
 	public static void car() {
 		System.out.println("Scorpio");
 	}
@@ -10,8 +16,11 @@ public class exp {
 		System.out.println("905 BUS");
 	}
 	public static void main(String[] args) {
+		tree();
+		flower();
 		car();
 		bus();
+		
 	}
 	
 }
